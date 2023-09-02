359ea4d072190271b6fc7e903d9a4473129cb285
amanuel <amanuelyihunie27@gmail.com>
2023-09-02 14:39:37 +0300

    [Backend] add authorization

---
+++ Backend/src/middlewares/auth.ts
@@ -0,0 +1,43 @@
+//auth middleware
+
+import { Request, Response, NextFunction } from "express";
+import { User } from "../models";
+
+import jwt from "jsonwebtoken";
+import { config } from "../config/config";
+
+export const protect = async (req: any, res: Response, next: NextFunction) => {
+  try {
+    //get token from header bearer token
+    const token = req.header("Authorization")?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        status: "fail",
+        message: "No token, authorization denied",
+      });
+    }
+
+    //verify token
+    const decoded: any = jwt.verify(token, config.jwt.secret!);
+    if (!decoded) {
+      return res.status(401).json({
+        status: "fail",
+        message: "Token is not valid",
+      });
+    }
+    //check if user exists
+    const user = await User.findByPk(decoded.id);
+    if (!user) {
+      return res.status(401).json({
+        status: "fail",
+        message: "User not found",
+      });
+    }
+    req.user = user;
+  } catch (err) {
+    res.status(401).json({
+      status: "fail",
+      message: "Not authorized to access this route",
+    });
+  }
+};
