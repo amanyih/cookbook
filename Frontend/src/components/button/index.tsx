f4346a53de8045dcaee17a7ec103a9dbcfb8120c
amanuel <amanuelyihunie27@gmail.com>
2023-09-18 23:25:15 +0300

    []

--- Frontend/src/components/button/index.tsx
+++ Frontend/src/components/button/index.tsx
@@ -17,13 +17,13 @@ const Button: React.FC<ButtonProps> = (props) => {
       className={`
       ${
         props.disabled
-          ? "bg-gray-300 dark:bg-gray-700 text-gray-700 dark:text-gray-100 cursor-not-allowed"
+          ? "bg-gray-300 dark:bg-gray-700 text-gray-200 dark:text-gray-100 cursor-not-allowed"
           : props.background
           ? props.background
           : "bg-primary-400 dark:bg-red-500 hover:bg-red-500 dark:hover:bg-primary-400"
       }
       ${
-        props.textcolor ?? "dark:text-gray-300 text-gray-900"
+        props.textcolor ?? "dark:text-gray-300 text-gray-200"
       } px-5 py-2 rounded-md transition duration-300 ease-in-out  ${
         props.className
       }`}
